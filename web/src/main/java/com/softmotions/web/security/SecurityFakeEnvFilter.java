382d394c61620f3a7118d32d96bc2e9dbb31be4f
Антон Адаманский <adamansky@gmail.com>
2014-04-15 12:17:38 +0700

    added SecurityFakeEnvFilter

---
+++ web/src/main/java/com/softmotions/web/security/SecurityFakeEnvFilter.java
@@ -0,0 +1,84 @@
+package com.softmotions.web.security;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.security.Principal;
+
+/**
+ * @author Adamansky Anton (adamansky@gmail.com)
+ */
+public class SecurityFakeEnvFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityFakeEnvFilter.class);
+
+    private WSUserDatabase db;
+
+    private String username;
+
+    public void init(FilterConfig conf) throws ServletException {
+        String dbJndiName = conf.getInitParameter("dbJndiName");
+        if (!StringUtils.isBlank(dbJndiName)) {
+            try {
+                InitialContext ctx = new InitialContext();
+                db = (WSUserDatabase) ctx.lookup(dbJndiName);
+            } catch (NamingException e) {
+                log.error("", e);
+                throw new ServletException(e);
+            }
+        }
+        username = conf.getInitParameter("username");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (db != null && username != null) {
+            request = new SecurityHttpServletRequestWrapper(db, username, (HttpServletRequest) request);
+        }
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        this.db = null;
+        this.username = null;
+    }
+
+    private static class SecurityHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+        private final WSUserDatabase db;
+
+        private final String username;
+
+        private SecurityHttpServletRequestWrapper(WSUserDatabase db, final String username,
+                                                  HttpServletRequest request) {
+            super(request);
+            this.db = db;
+            this.username = username;
+        }
+
+        public String getRemoteUser() {
+            return username;
+        }
+
+        public Principal getUserPrincipal() {
+            return db.findUser(username);
+        }
+
+        public boolean isUserInRole(String role) {
+            WSUser wsuser = db.findUser(username);
+            WSRole wsrole = db.findRole(role);
+            return (wsuser != null && wsrole != null && wsuser.isInRole(wsrole));
+        }
+    }
+}
