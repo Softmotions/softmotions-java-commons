4c8a92bf68df643206b44ae8ce05650d6cd2f126
Антон Адаманский <adamansky@gmail.com>
2014-09-27 02:25:41 +0700

    -

---
+++ weboot/src/main/java/com/softmotions/weboot/mb/MBSqlSessionListenerSupport.java
@@ -0,0 +1,19 @@
+package com.softmotions.weboot.mb;
+
+/**
+ * @author Adamansky Anton (adamansky@gmail.com)
+ */
+public class MBSqlSessionListenerSupport implements MBSqlSessionListener {
+
+    public void commit(boolean success) {
+
+    }
+
+    public void rollback() {
+
+    }
+
+    public void close(boolean success) {
+
+    }
+}
