51ddc9bab7ace27f051d586b108df4bb26ea2658
Anton Adamansky <adamansky@gmail.com>
2016-05-31 22:46:08 +0600

    - added RFC5987 encoder/decoder

--- utils/src/main/java/com/softmotions/commons/string/EscapeHelper.java
+++ utils/src/main/java/com/softmotions/commons/string/EscapeHelper.java
@@ -1,12 +1,16 @@
 package com.softmotions.commons.string;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -203,7 +207,6 @@ public final class EscapeHelper {
         return buffer.toString();
     }
 
-
     @Nonnull
     public static String escapeJSON(String input) {
         if (input == null) {
@@ -256,6 +259,62 @@ public final class EscapeHelper {
         return sw.toString();
     }
 
+    /**
+     * Utility for encoding and decoding values according to RFC 5987. Assumes the
+     * caller already knows the encoding scheme for the value.
+     */
+    private static final Pattern RFC5987_ENCODED_VALUE_PATTERN = Pattern.compile("%[0-9a-f]{2}|\\S",
+                                                                                 Pattern.CASE_INSENSITIVE);
+
+    public static String encodeRFC5987(final String s) throws UnsupportedEncodingException {
+        return encodeRFC5987(s, "UTF-8");
+    }
+
+    // http://stackoverflow.com/questions/11302361/ (continued next line)
+    // handling-filename-parameters-with-spaces-via-rfc-5987-results-in-in-filenam
+    @Nonnull
+    public static String encodeRFC5987(final String s, String encoding) throws UnsupportedEncodingException {
+        final byte[] rawBytes = s.getBytes(encoding);
+        final int len = rawBytes.length;
+        final StringBuilder sb = new StringBuilder(len << 1);
+        final char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                               'a', 'b', 'c', 'd', 'e', 'f'};
+        final byte[] attributeChars = {'!', '#', '$', '&', '+', '-', '.', '0',
+                                       '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+                                       'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                                       'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '^', '_', '`', 'a',
+                                       'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                       'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|',
+                                       '~'};
+        for (final byte b : rawBytes) {
+            if (Arrays.binarySearch(attributeChars, b) >= 0) {
+                sb.append((char) b);
+            } else {
+                sb.append('%');
+                sb.append(digits[0x0f & (b >>> 4)]);
+                sb.append(digits[b & 0x0f]);
+            }
+        }
+        return sb.toString();
+    }
+
+    @Nonnull
+    public static String decodeRFC5987(String s, String encoding)
+            throws UnsupportedEncodingException {
+        Matcher matcher = RFC5987_ENCODED_VALUE_PATTERN.matcher(s);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while (matcher.find()) {
+            String matched = matcher.group();
+            if (matched.startsWith("%")) {
+                Integer value = Integer.parseInt(matched.substring(1), 16);
+                bos.write(value);
+            } else {
+                bos.write(matched.charAt(0));
+            }
+        }
+
+        return new String(bos.toByteArray(), encoding);
+    }
 
     private EscapeHelper() {
     }
