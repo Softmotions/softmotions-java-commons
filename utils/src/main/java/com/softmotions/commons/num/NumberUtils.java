0949d259be961e66c97e1deee078bae98317ccdc
Anton Adamansky <adamansky@gmail.com>
2017-01-29 17:57:13 +0700

    - NumberUtils.number2Double

--- utils/src/main/java/com/softmotions/commons/num/NumberUtils.java
+++ utils/src/main/java/com/softmotions/commons/num/NumberUtils.java
@@ -14,12 +14,24 @@ public class NumberUtils {
         return (n != null) ? n.doubleValue() : defval;
     }
 
+    public static double number2Double(Number n) {
+        return (n != null) ? n.doubleValue() : 0.0d;
+    }
+
     public static long number2Long(Number n, long defval) {
         return (n != null) ? n.longValue() : defval;
     }
 
+    public static long number2Long(Number n) {
+        return (n != null) ? n.longValue() : 0;
+    }
+
     public static int number2Int(Number n, int defval) {
         return (n != null) ? n.intValue() : defval;
+    }
+
+    public static int number2Int(Number n) {
+        return (n != null) ? n.intValue() : 0;
     }
 
     public static boolean number2Boolean(Number n) {
