feb563ce974ba7fcca49bb44ff7c3110b3911576
Anton Adamansky <adamansky@gmail.com>
2018-07-03 23:27:57 +0700

    - removed mime package

--- utils/src/main/java/com/softmotions/commons/mime/MimeTypeDetector.java
+++
@@ -1,7 +0,0 @@
-package com.softmotions.commons.mime;
-
-/**
- * @author Adamansky Anton (adamansky@softmotions.com)
- */
-public class MimeTypeDetector {
-}
--- utils/src/main/java/com/softmotions/commons/mime/README.md
+++
@@ -1,2 +0,0 @@
- http://sourceforge.net/projects/mime-util
- 
