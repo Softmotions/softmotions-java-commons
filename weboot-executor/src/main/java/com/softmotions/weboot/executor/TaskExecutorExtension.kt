523b24fe6f252eb52f68b287d12cd4807feed03c
Anton Adamansky <adamansky@gmail.com>
2018-07-12 02:08:40 +0700

    - Added executor fun in context of weboot env

--- weboot-executor/src/main/java/com/softmotions/weboot/executor/TaskExecutorExtension.kt
+++ weboot-executor/src/main/java/com/softmotions/weboot/executor/TaskExecutorExtension.kt
@@ -12,7 +12,7 @@ import java.util.concurrent.Future
  * @author Adamansky Anton (adamansky@softmotions.com)
  */
 public fun executor(name: String? = null, block: () -> Unit): Future<*> {
-    val injector = JVMResources.get<Injector>("com.softmotions.weboot.WBServletListener.Injector")
+    val injector = JVMResources.getOrFail<Injector>("com.softmotions.weboot.WBServletListener.Injector")
     val executor = if (name == null) {
         injector.getInstance(TaskExecutor::class.java)
     } else {
