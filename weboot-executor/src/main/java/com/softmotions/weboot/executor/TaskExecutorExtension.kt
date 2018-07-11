2dd18dc5e3f910c5f73fa41104ba7ba5883c8c61
Anton Adamansky <adamansky@gmail.com>
2018-07-12 02:02:14 +0700

    - Added executor fun in context of weboot env

---
+++ weboot-executor/src/main/java/com/softmotions/weboot/executor/TaskExecutorExtension.kt
@@ -0,0 +1,22 @@
+package com.softmotions.weboot.executor
+
+import com.google.inject.Injector
+import com.google.inject.Key
+import com.google.inject.name.Names
+import com.softmotions.commons.JVMResources
+import java.util.concurrent.Future
+
+/**
+ * Kotlin executor
+ *
+ * @author Adamansky Anton (adamansky@softmotions.com)
+ */
+public fun executor(name: String? = null, block: () -> Unit): Future<*> {
+    val injector = JVMResources.get<Injector>("com.softmotions.weboot.WBServletListener.Injector")
+    val executor = if (name == null) {
+        injector.getInstance(TaskExecutor::class.java)
+    } else {
+        injector.getInstance(Key.get(TaskExecutor::class.java, Names.named(name)))
+    }
+    return executor.submit(block)
+}
--- weboot-executor/src/main/java/com/softmotions/weboot/executor/TaskExecutorModule.java
+++ weboot-executor/src/main/java/com/softmotions/weboot/executor/TaskExecutorModule.java
@@ -6,6 +6,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
