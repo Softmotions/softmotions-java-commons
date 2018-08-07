05afb0b6879656e59611ab26e0b3897f4667c00a
Anton Adamansky <adamansky@gmail.com>
2018-08-07 11:51:44 +0700

    - Cayenne 4.1.M2

--- weboot-cayenne/src/main/java/com/softmotions/weboot/cayenne/DumbCayenneContext.java
+++ weboot-cayenne/src/main/java/com/softmotions/weboot/cayenne/DumbCayenneContext.java
@@ -1,26 +1,23 @@
 package com.softmotions.weboot.cayenne;
 
-import org.apache.cayenne.CayenneContext;
 import org.apache.cayenne.DataChannel;
 import org.apache.cayenne.PersistenceState;
 import org.apache.cayenne.Persistent;
+import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.access.ObjectStore;
 
 /**
  * @author Adamansky Anton (adamansky@gmail.com)
  */
-public class DumbCayenneContext extends CayenneContext {
+public class DumbCayenneContext extends DataContext {
     /**
      * Creates a new CayenneContext with no channel and disabled graph events.
      */
     public DumbCayenneContext() {
     }
 
-    private DumbCayenneContext(DataChannel channel) {
-        super(channel);
-    }
-
-    private DumbCayenneContext(DataChannel channel, boolean changeEventsEnabled, boolean lifecyleEventsEnabled) {
-        super(channel, changeEventsEnabled, lifecyleEventsEnabled);
+    public DumbCayenneContext(DataChannel channel, ObjectStore objectStore) {
+        super(channel, objectStore);
     }
 
     @Override
