d3cc112e4f5a2f0542d527a72c0116f4f537de47
Anton Adamansky <adamansky@gmail.com>
2018-08-30 17:18:49 +0700

    - JWTAuthenticationFilter

--- weboot-security/src/main/java/com/softmotions/weboot/security/jwt/JWTAuthenticationFilter.java
+++ weboot-security/src/main/java/com/softmotions/weboot/security/jwt/JWTAuthenticationFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.apache.shiro.web.util.WebUtils;
@@ -18,18 +19,23 @@ public class JWTAuthenticationFilter extends AuthenticatingFilter {
     protected AuthenticationToken createToken(ServletRequest sreq, ServletResponse sresp) throws Exception {
         HttpServletRequest req = WebUtils.toHttp(sreq);
         String jwt = req.getHeader("Authorization");
-        if (jwt != null && jwt.startsWith("Bearer ")) {
-            jwt = jwt.substring(jwt.indexOf(' ')).trim();
-            return new JWTAuthenticationToken(jwt);
-        } else {
-            return new JWTAuthenticationToken("");
-        }
+        if (jwt == null) throw new AuthenticationException("Missing 'Authorization' HTTP header");
+        jwt = jwt.substring(jwt.indexOf(' ')).trim();
+        return new JWTAuthenticationToken(jwt);
     }
 
     @Override
     protected boolean onAccessDenied(ServletRequest sreq, ServletResponse sresp) throws Exception {
+        HttpServletRequest req = WebUtils.toHttp(sreq);
         HttpServletResponse resp = WebUtils.toHttp(sresp);
-        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return false;
+        boolean loggedIn = false;
+        String jwt = req.getHeader("Authorization");
+        if (jwt != null && jwt.startsWith("Bearer ")) {
+            loggedIn = executeLogin(sreq, sresp);
+        }
+        if (!loggedIn) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        return loggedIn;
     }
 }
