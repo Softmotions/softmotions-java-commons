36b075ab5fa409f077ea67142198971610223bf5
Anton Adamansky <adamansky@gmail.com>
2018-09-22 19:59:32 +0700

    - WS async

---
+++ weboot-jaxrs/src/main/java/com/softmotions/weboot/jaxrs/ws/AbstractWSMessage.java
@@ -0,0 +1,7 @@
+package com.softmotions.weboot.jaxrs.ws;
+
+/**
+ * @author Adamansky Anton (adamansky@softmotions.com)
+ */
+public class AbstractWSMessage {
+}
---
+++ weboot-jaxrs/src/main/java/com/softmotions/weboot/jaxrs/ws/StringWSMessage.java
@@ -0,0 +1,10 @@
+package com.softmotions.weboot.jaxrs.ws;
+
+/**
+ *
+ * https://svn.apache.org/viewvc/tomcat/trunk/webapps/examples/WEB-INF/classes/websocket/drawboard/Client.java?view=markup
+ * 
+ * @author Adamansky Anton (adamansky@softmotions.com)
+ */
+public class StringWSMessage extends AbstractWSMessage {
+}
