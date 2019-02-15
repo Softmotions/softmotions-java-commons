df90a56ce015b3c6c1d6f18685c45458684c52ec
alaze <dugalev@softmotions.com>
2019-02-15 15:55:10 +0700

    #6-aws-s3-rep

--- weboot-repository/src/main/java/com/softmotions/weboot/repository/s3/AWSS3Repository.kt
+++ weboot-repository/src/main/java/com/softmotions/weboot/repository/s3/AWSS3Repository.kt
@@ -10,29 +10,17 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 import com.softmotions.commons.ServicesConfiguration
 import com.softmotions.kotlin.loggerFor
+import com.softmotions.kotlin.toPath
 import com.softmotions.weboot.repository.WBRepository
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.URI
-
-// 1 понял 2 тех план 3 минимальная реализация + мин тест 4. фантики?
-
-
-//fun acceptUri(uri: URI): Boolean
-//
-//fun persist(input: InputStream, fname: String? = null): URI
-//
-//fun remove(uri: URI): Boolean
-//
-//fun transferTo(uri: URI, output: OutputStream)
+import kotlin.NullPointerException
 
 @Singleton
 class AWSS3Repository
 @Inject
 constructor(env: ServicesConfiguration) : WBRepository {
-    /**
-     * s3://<bucketname>/<uuid>
-     */
 
     companion object {
         private val log = loggerFor()
@@ -40,10 +28,7 @@ constructor(env: ServicesConfiguration) : WBRepository {
 
     private var s3: AmazonS3
 
-    // todo Есть ли S3 embedded server
-    // todo Mokito
-
-    private var bucket: Bucket // todo
+    private var bucket: Bucket
 
     init {
         val xcfg = env.xcfg()
@@ -62,7 +47,7 @@ constructor(env: ServicesConfiguration) : WBRepository {
         }.build()
 
         bucket = if (s3.doesBucketExistV2(bucketName)) {
-            Bucket(bucketName) // TODO
+            s3.listBuckets().last { bucket -> bucket.name == bucketName }
         } else {
             s3.createBucket(bucketName)
         }
@@ -72,16 +57,16 @@ constructor(env: ServicesConfiguration) : WBRepository {
         return "s3" == uri.scheme && uri.host == bucket.name
     }
 
-    override fun fetchFileName(uri: URI): String = uri.path
+    override fun fetchFileName(uri: URI): String = uri.path.toPath().fileName.toString()
 
     override fun persist(input: InputStream, fname: String): URI {
         s3.putObject(bucket.name, fname, input, null)
-        return URI("s3", "${bucket.name}/$fname", null)
+        return URI("s3", bucket.name, "/$fname", null)
     }
 
     override fun remove(uri: URI): Boolean {
-        return if (s3.doesObjectExist(bucket.name, uri.schemeSpecificPart)) {
-            s3.deleteObject(bucket.name, uri.schemeSpecificPart)
+        return if (s3.doesObjectExist(bucket.name, fetchFileName(uri))) {
+            s3.deleteObject(bucket.name, fetchFileName(uri))
             true
         } else {
             false
@@ -89,8 +74,13 @@ constructor(env: ServicesConfiguration) : WBRepository {
     }
 
     override fun transferTo(uri: URI, output: OutputStream) {
-        s3.getObject(bucket.name, uri.schemeSpecificPart).use { s3o ->
-            s3o.objectContent.transferTo(output)
+        val key = fetchFileName(uri)
+        if (s3.doesObjectExist(bucket.name, key)) {
+            s3.getObject(bucket.name, key).use { s3o ->
+                s3o.objectContent.transferTo(output)
+            }
+        } else {
+            throw NullPointerException("Missing repository key: '$key'")
         }
     }
 }
--- weboot-repository/src/test/java/com/softmotions/weboot/repository/TestS3Repository.kt
+++ weboot-repository/src/test/java/com/softmotions/weboot/repository/TestS3Repository.kt
@@ -9,6 +9,8 @@ import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 import java.io.ByteArrayOutputStream
+import java.lang.NullPointerException
+import java.net.URI
 import java.util.*
 
 @Test
@@ -16,29 +18,96 @@ class TestS3Repository {
 
     private val cfgLocation: String = "conf/repository-test-configuration.xml"
     private lateinit var injector: Injector
+    private lateinit var env: TestEnv
     private lateinit var s3Mock: S3Mock
 
     @BeforeClass
     fun before() {
         s3Mock = S3Mock.Builder().withPort(8001).withInMemoryBackend().build()
         s3Mock.start()
-        val env = TestEnv(cfgLocation)
+        env = TestEnv(cfgLocation)
         injector = Guice.createInjector(Stage.PRODUCTION, env)
     }
 
     @Test
-    fun test1() {
+    fun test1Upload() {
         val rep = injector.getInstance(WBRepository::class.java)
-
         val data = "TEST TEXT"
+        val key = "key1"
 
         val uri = data.byteInputStream().use { input ->
-            rep.persist(input, "key")
+            rep.persist(input, key)
         }
+
+        // test filename
+        Assert.assertEquals(key, rep.fetchFileName(uri))
 
         ByteArrayOutputStream().use { output ->
             rep.transferTo(uri, output)
             Assert.assertEquals(data.toByteArray(), output.toByteArray())
+        }
+
+        // test uri
+        Assert.assertTrue(rep.acceptUri(uri))
+        Assert.assertTrue(rep.acceptUri(URI("s3", env.xcfg()["repository.s3.bucket"], null, "badobject")))
+        Assert.assertFalse(rep.acceptUri(URI("s3", "badbucket", null, "badobject")))
+    }
+
+    @Test
+    fun test2Replace() {
+        val rep = injector.getInstance(WBRepository::class.java)
+        val data1 = "TEST TEXT"
+        val data2 = "TEST TEXT 2"
+        val key = "key2"
+
+        val uri1 = data1.byteInputStream().use { input ->
+            rep.persist(input, key)
+        }
+
+        ByteArrayOutputStream().use { output ->
+            rep.transferTo(uri1, output)
+            Assert.assertEquals(data1.toByteArray(), output.toByteArray())
+        }
+
+        val uri2 = data2.byteInputStream().use { input ->
+            rep.persist(input, key)
+        }
+
+        // test, uri no change
+        Assert.assertEquals(uri1, uri2)
+
+        ByteArrayOutputStream().use { output ->
+            rep.transferTo(uri1, output)
+            val res = output.toByteArray()
+            Assert.assertNotEquals(data1.toByteArray(), res)
+            Assert.assertEquals(data2.toByteArray(), res)
+        }
+    }
+
+    @Test
+    fun test3Remove() {
+        val rep = injector.getInstance(WBRepository::class.java)
+        val data = "TEST TEXT"
+        val key = "key3"
+
+        val uri = data.byteInputStream().use { input ->
+            rep.persist(input, key)
+        }
+
+        // check data
+        ByteArrayOutputStream().use { output ->
+            rep.transferTo(uri, output)
+            Assert.assertEquals(data.toByteArray(), output.toByteArray())
+        }
+
+        // remove data
+        Assert.assertTrue(rep.remove(uri))
+
+        // check data
+        ByteArrayOutputStream().use { output ->
+            Assert.assertThrows(NullPointerException::class.java) {
+                rep.transferTo(uri, output)
+            }
         }
     }
 
