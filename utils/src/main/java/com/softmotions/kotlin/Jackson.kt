c94e9279b1ab76893c9c12d9a5a3d2eab0082f8a
Anton Adamansky <adamansky@gmail.com>
2019-07-30 11:40:23 +0700

    * Added ObjectMapper.obj & ObjectMapper.arr

--- utils/src/main/java/com/softmotions/kotlin/Jackson.kt
+++ utils/src/main/java/com/softmotions/kotlin/Jackson.kt
@@ -1,6 +1,8 @@
 package com.softmotions.kotlin
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import java.util.*
@@ -22,4 +24,16 @@ fun ObjectNode.text(path: String, default: String = ""): String {
 
 fun ObjectNode.long(path: String, default: Long = 0): Long {
     return this.path(path).asLong(default)
+}
+
+fun ObjectMapper.obj(block: ObjectNode.() -> Unit): ObjectNode {
+    val n = createObjectNode()
+    block(n)
+    return n
+}
+
+fun ObjectMapper.arr(block: ArrayNode.() -> Unit): ArrayNode {
+    val n = createArrayNode().also(block)
+    block(n)
+    return n
 }
